b79d9c692b58f0398e3bada96103f2136d3ac0a1
승원 <milk5590@naver.com>
2016-02-22 01:28:34 +0900

    간단한 문구 추가

--- src/main/java/milk/worldeditor/WorldEditor.java
+++ src/main/java/milk/worldeditor/WorldEditor.java
@@ -1,17 +1,18 @@
 package milk.worldeditor;
 
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.TextFormat;
 
 public class WorldEditor extends PluginBase{
 
     @Override
     public void onEnable(){
-
+        this.getServer().getLogger().info(TextFormat.GOLD + "[WorldEditor]플러그인이 활성화 되었습니다");
     }
 
     @Override
     public void onDisable(){
-
+        this.getServer().getLogger().info(TextFormat.GOLD + "[WorldEditor]플러그인이 비활성화 되었습니다");
     }
 
 }
