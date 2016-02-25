5212c9442c55d0389ca09af51fbf92bb6c5b9ca7
승원 <milk5590@naver.com>
2016-02-26 01:08:47 +0900

    잘못되었던 문구 수정

--- src/main/java/milk/worldeditor/WorldEditor.java
+++ src/main/java/milk/worldeditor/WorldEditor.java
@@ -7,12 +7,12 @@ public class WorldEditor extends PluginBase{
 
     @Override
     public void onEnable(){
-        this.getServer().getLogger().info(TextFormat.GOLD + "[WorldEditor]플러그인이 활성화 되었습니다");
+        this.getServer().getLogger().info(TextFormat.GOLD + "[WorldEditor]Plugin has been enabled");
     }
 
     @Override
     public void onDisable(){
-        this.getServer().getLogger().info(TextFormat.GOLD + "[WorldEditor]플러그인이 비활성화 되었습니다");
+        this.getServer().getLogger().info(TextFormat.GOLD + "[WorldEditor]Plugin has been disabled");
     }
 
 }
